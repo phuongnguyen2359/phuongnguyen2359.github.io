67c19e4c284e5fe6b11655e3b097a1c9b6621abd
Phuong Nguyen <nguyenphuong.it242@gmail.com>
2019-05-30 14:11:26 +0700

    #

---
+++ precache-manifest.8b7df023afd956395641586d809679bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8283692d9570f2133fa84825a311f03c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8153ef360d19d762022",
+    "url": "/static/css/main.c6688633.chunk.css"
+  },
+  {
+    "revision": "5671dbe211a4a4178c40",
+    "url": "/static/js/2.5ae0bb80.chunk.js"
+  },
+  {
+    "revision": "c8153ef360d19d762022",
+    "url": "/static/js/main.7813e1bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "822e2bb2c438031446d36b0487bffe15",
+    "url": "/static/media/loading.822e2bb2.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
