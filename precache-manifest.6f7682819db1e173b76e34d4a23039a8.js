b1c50dd717882820bce40a34caa1bc8eec75152c
Pj Nguyen <nguyenphuong.it242@gmail.com>
2019-06-05 00:33:49 +0700

    adjust video id

---
+++ precache-manifest.6f7682819db1e173b76e34d4a23039a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc74d927dfbcf2316366d2539e2ba101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b273d3b93dd87154574",
+    "url": "/static/css/main.e41f68d4.chunk.css"
+  },
+  {
+    "revision": "9e89fa5940a56280d285",
+    "url": "/static/js/2.44a482b1.chunk.js"
+  },
+  {
+    "revision": "8b273d3b93dd87154574",
+    "url": "/static/js/main.bc8f44fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "822e2bb2c438031446d36b0487bffe15",
+    "url": "/static/media/loading.822e2bb2.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
