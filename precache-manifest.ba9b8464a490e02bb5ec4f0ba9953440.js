ce6dea42e5e1ddfac5c3e87f1e0a696ab1f25e86
Pj Nguyen <nguyenphuong.it242@gmail.com>
2019-12-13 12:33:16 +0700

    new version 2nd

---
+++ precache-manifest.ba9b8464a490e02bb5ec4f0ba9953440.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b413ad04d61aabfbc8ea00abade20d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f2bb6e06bf4ef06395",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "8255934788cf1ff78624",
+    "url": "/static/css/main.40f3e28f.chunk.css"
+  },
+  {
+    "revision": "e3f2bb6e06bf4ef06395",
+    "url": "/static/js/2.8b3afe27.chunk.js"
+  },
+  {
+    "revision": "4b9ab498671eb0676f2994658563ce88",
+    "url": "/static/js/2.8b3afe27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8255934788cf1ff78624",
+    "url": "/static/js/main.868e3d7d.chunk.js"
+  },
+  {
+    "revision": "4186af9fcc68f1a7fbec",
+    "url": "/static/js/runtime-main.f190fd1f.js"
+  },
+  {
+    "revision": "822e2bb2c438031446d36b0487bffe15",
+    "url": "/static/media/loading.822e2bb2.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
